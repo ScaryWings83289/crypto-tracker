e7c96caeab8408a4543690d59edef1dc5ea19974
Divyankar Bhargav <divyankar.bhargav@uniacco.com>
2024-03-30 19:28:25 +0530

    Feat: Initialize repository with react + vite + typescript and added absolute import configuration

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+//* Packages Imports */
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
+
+export default defineConfig({
+  base: "./",
+  resolve: {
+    alias: {
+      "@Assets": path.resolve(__dirname, "./src/assets"),
+      "@Components": path.resolve(__dirname, "./src/components"),
+      "@Context": path.resolve(__dirname, "./src/context"),
+      "@Pages": path.resolve(__dirname, "./src/pages"),
+      "@Src": path.resolve(__dirname, "./src"),
+      "@Styles": path.resolve(__dirname, "./src/styles"),
+      "@Utils": path.resolve(__dirname, "./src/utils"),
+    },
+  },
+  plugins: [react(), tsconfigPaths()],
+});
