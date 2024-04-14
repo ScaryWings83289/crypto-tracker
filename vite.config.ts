3a72ce16e507ca1e875d61e20377768f5bc87afa
Divyankar Bhargav <divyankar.bhargav@uniacco.com>
2024-04-14 18:58:02 +0530

    Fix: Updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
 
 export default defineConfig({
   base: "./",
   resolve: {
     alias: {
-      "@Assets": "./src/assets",
-      "@Components": "./src/components",
-      "@Context": "./src/context",
-      "@Pages": "./src/pages",
-      "@Src": "./src",
-      "@Utils": "./src/utils",
-      "@Data": "./src/data",
+      "@Assets": path.resolve(new URL("./src/assets", import.meta.url).pathname),
+      "@Components": path.resolve(new URL("./src/components", import.meta.url).pathname),
+      "@Context": path.resolve(new URL("./src/context", import.meta.url).pathname),
+      "@Pages": path.resolve(new URL("./src/pages", import.meta.url).pathname),
+      "@Src": path.resolve(new URL("./src", import.meta.url).pathname),
+      "@Utils": path.resolve(new URL("./src/utils", import.meta.url).pathname),
+      "@Data": path.resolve(new URL("./src/data", import.meta.url).pathname),
     },
   },
   plugins: [react(), tsconfigPaths()],
